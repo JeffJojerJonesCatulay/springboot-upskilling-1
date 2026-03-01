e660f418c23a23b19b84fd4fb06fdd2b3a806acb
Jeff Jojer Jones E. Catulay <catulay.programming.acc@gmail.com>
2026-03-01 12:49:33 +0800

    intial commit

---
+++ src/test/java/com/jecatulay/springboot_upskilling_1/SpringbootUpskilling1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jecatulay.springboot_upskilling_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUpskilling1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
